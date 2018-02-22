c372b00572e783fc156396eba355699e87ee107d
w3s7y <ben.west.500@gmail.com>
2018-02-22 18:09:34 +0000

    More large scale refactoring and adding more profiling code.

--- etl.py
+++ etl.py
@@ -12,8 +12,10 @@ logger.addHandler(handle)
 
 
 class EtlPipeline:
+    """Lazy loads the csv, then each 'transform_and_load_*' method runs the transform/enrich and load into the DB."""
     def __init__(self, gp_address_file, prescriptions_file, drug_subs_file, chunk_size, google_token,
                  db_host, db_username, db_password, db_name, db_schema):
+
         self.db_engine = sqlalchemy.create_engine(
             'postgresql+psycopg2://{}:{}@{}/{}'.format(db_username, db_password, db_host, db_name))
         self.schema = db_schema
@@ -25,45 +27,57 @@ class EtlPipeline:
 
     def transform_and_load_addresses(self):
         logger.info('Running address transform.')
+        num = 1
         for chunk in self.addresses:
             chunk = chunk.drop(columns=['yrmon'])
             for field in ['practice_name', 'address_1', 'address_2', 'address_3', 'address_4']:
                 chunk[field] = chunk[field].map(lambda x: x.strip().lower().capitalize())
-
+            print('loading address chunk {} into database'.format(num))
+            num += 1
             # TODO Adding 2 new fields 'latitude' and 'longitude' gathered from google geo location API
             self.load('addresses', chunk)
 
     def transform_and_load_prescriptions(self):
+        num = 1
         for chunk in self.prescriptions:
             chunk = chunk.drop(columns=['period', 'blank'])
             for field in ['drug_name']:
                 chunk[field] = chunk[field].map(lambda x: x.strip())
+            num += 1
+            if num == 4:
+                break
             self.load('prescriptions', chunk)
 
     def transform_and_load_subs(self):
+        num = 1
         for chunk in self.substitutions:
             chunk = chunk.drop(columns=['blank'])
             for field in ['chemical_code', 'chemical_name']:
                 chunk[field] = chunk[field].map(lambda x: x.strip())
+            print('loading subs chunk {} into database'.format(num))
+            num += 1
             self.load('substitutions', chunk)
 
     def load(self, table, chunk):
         chunk.to_sql(
             table, self.db_engine, schema=self.schema, if_exists='append')
 
+    def get_geo_location(self, address):
+        pass
 
-class etl_controller:
+
+class EtlController:
     """Defaults and stuff, basic logic control."""
 
-    def __init__(self):
+    def __init__(self, chunkz):
         # Vars for the Extractor
-        gp_addresses = 'T201710ADDR+BNFT.CSV'
-        prescriptions_data = 'T201710PDPI+BNFT.CSV'
-        drug_substitutions = 'T201710CHEM+SUBS.CSV'
-        chunk_size = 1000
+        gp_addresses = os.environ.get('ADDRESS_FILE', default='T201710ADDR+BNFT.CSV')
+        prescriptions_data = os.environ.get('PRESCRIPTIONS_FILE', default='T201710PDPI+BNFT.CSV')
+        drug_substitutions = os.environ.get('SUBS_FILE', default='T201710CHEM+SUBS.CSV')
+        chunk_size = chunkz
 
         # Vars for the data transformer & enrichment
-        google_token = os.environ.get('GOOGLE_API_TOKEN')
+        google_token = os.environ.get('GOOGLE_API_TOKEN', default='####################')
 
         # Vars for the loader
         db_user = os.environ.get('DB_USER', default='ubuntu')
@@ -76,6 +90,6 @@ class etl_controller:
                                     db_host, db_user, db_pass, db_name, db_schema)
 
     def run_pipe(self):
-        self.pipeline.transform_and_load_addresses()
+        # self.pipeline.transform_and_load_addresses()
         self.pipeline.transform_and_load_prescriptions()
-        self.pipeline.transform_and_load_subs()
+        # self.pipeline.transform_and_load_subs()
--- profiler.py
+++ profiler.py
@@ -1,9 +1,19 @@
 import cProfile
 import etl
 import pstats
+import timeit
 
-# Profile the csv load & transform.
-cProfile.run("etl.etl_controller().run_pipe()", 'prescription_stats.profile')
-p = pstats.Stats('prescription_stats.profile')
-p.sort_stats('cumulative').print_stats(50)
 
+
+def full_load_profile(chunk_size):
+    # Profiles the etl pipeline.
+    cProfile.run("etl.EtlController({}).run_pipe()".format(chunk_size), 'prescription_stats.profile')
+    p = pstats.Stats('prescription_stats.profile')
+    p.sort_stats('cumulative').print_stats(50)
+
+def timed_etl_prescriptions_loads(chunk_sizes, iters):
+    for chunk_size in chunk_sizes:
+        time = timeit.repeat("e.run_pipe()", setup="import etl; e = etl.EtlController({})".format(chunk_size), number=iters)
+        print("Runtime ({} iters) for a chunk size of {} was: {}".format(iters, chunk_size, time))
+
+timed_etl_prescriptions_loads([100, 1000, 10000], 1)
